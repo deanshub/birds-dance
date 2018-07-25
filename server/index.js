e5dc4efcfa74422bf6d548668b6cb47d4dcb78ad
Lior Shub <liorbentov@gmail.com>
2018-07-25 08:13:46 -0400

    Minor changes

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import path from 'path'
 const app = express()
 app.use(bodyParser.json())
 
-if (process.env.NODE_ENV==='development'){
+if (process.env.NODE_ENV === 'development'){
   app.use(devMiddleware())
   app.use(hotMiddleware())
 }
@@ -16,5 +16,5 @@ const STATIC_FILES_DIRECTORY = path.join(__dirname,'../client/static')
 app.use(express.static(STATIC_FILES_DIRECTORY))
 
 app.listen(3000, () => {
-  console.log('Joe app listening on port 3000!')
+  console.log('App listening on port 3000!')
 })
